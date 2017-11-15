27c3b4a9911d905b04b5163263f9fdbfe949077b
mshimada <mshimada2@mail.csuchico.edu>
2017-11-14 23:58:42 -0800

    done

--- main.cpp
+++ main.cpp
@@ -21,41 +21,41 @@ int ySize(Bitmap);
 int main()
 
 {
-    //variables
-    
-    Bitmap photo;
-       
-    int size;
-    
-    string picFile1;
-    
-    string picFile2;
-    
-    vector <string> picList;
-    
-    bool validBmp1;
-    
-    bool validBmp2;
-    
-    bool validBmp3;
-    
-//checks for the parameters and sees if the 
+  //variables
 
-    do     
-    
-    {
-    
+  Bitmap photo;
+
+  int size;
+
+  string picFile1;
+
+  string picFile2;
+
+  vector <string> picList;
+
+  bool validBmp1;
+
+  bool validBmp2;
+
+  bool validBmp3;
+
+  //checks for the parameters and sees if the 
+
+  do     
+
+  {
+
     cout<<"Enter bitmap files to combine"<<endl;
-    
+
     cin>>picFile2;
-    
+
     validBmp1 == correctBmp(picFile2);
 
     if (validBmp1 == false)
 
     {
 
-    cout<<"Error: File type is invalid or does not exist. Please try again. "<<endl;
+      cout<<"Error: File type is invalid or does not exist. Please try again. "<<endl;
 
     }
 
@@ -63,19 +63,19 @@ int main()
 
     {
 
-    picList.push_back(picFile2);
+      picList.push_back(picFile2);
 
     }
 
 
-    }
+  }
 
   while(validBmp1 == false);
 
-     //next while loop takes care of the other files that come after the first one  
-       
+  //next while loop takes care of the other files that come after the first one  
+
   while(picList.size() < 10 || picFile1 == "Done")
-       
+
   {
 
 
@@ -84,75 +84,72 @@ int main()
     {
       cout<<"Please enter another file or type in DONE to start combining files"<<endl;
       cin>>picFile1;
+    }
 
+    if(picFile1 == "DONE")
 
-      if(picFile1 == "DONE")
-
-      {
+    {
 
       cout<<"You have entered DONE. Start combining images"<<endl;
 
       break;
 
-      }
+    }
 
 
-      else if(picList.size() < 10 && picFile1 != "DONE")
+    else if(picList.size() < 10 && picFile1 != "DONE")
 
-      {
-        validBmp2 = correctBmp(picFile1);
-      
-      if(validBmp2 == true)
-        
-      {
+    {
+      validBmp2 = correctBmp(picFile1);
+    }
+    if(validBmp2 == true)
 
-        validBmp3 = correctSize(picFile1, picFile2);
+    {
 
-        if(validBmp3 == true)
+      validBmp3 = correctSize(picFile1, picFile2);
+    }
 
-        {
+    if(validBmp3 == true)
 
-        picList.push_back(picFile1);
+    {
+
+      picList.push_back(picFile1);
 
 
-        }
+    }
 
     else
 
     {
 
-    cout<<"Error: The file you entered is invalid and/or does not exist. Please try again. "<<endl;
+      cout<<"Error: The file you entered is invalid and/or does not exist. Please try again. "<<endl;
+
+    }
+
+    else
+
+    {
+
+      cout<<"The amount of files uploaded has reached its limit of 10"<<endl;
+
+      break;
 
 
     }
 
-   }
-
   }
 
-  else
+//converts it back into file from pixel and saves
+  vector <vector <Pixel> > compositeImage = avgRgb(picList);
 
-  {
+  if(picList.size() >= 2)
 
-    cout<<"The amount of files uploaded has reached its limit of 10"<<endl;
+  {   
 
-    break;
-
-
-  }
-}      
- 
-    
-    vector <vector <Pixel> > compositeImage = avgRgb(picList);
- 
-    if(picList.size() >= 2)
-    
-    {   
-    
     photo.fromPixelMatrix(compositeImage);
     photo.save("composite-mshimada2.bmp");
-    
-    }
+
+  }
 
   else
 
@@ -161,138 +158,137 @@ int main()
 
   }
 
-    return 0;
+  return 0;
 
 }
 
 bool correctBmp(string picFile)
 
- {
-    bool validBmp;
-    
-    Bitmap photo;
+{
+  bool validBmp;
 
-    picture.open(picFile);
+  Bitmap photo;
 
-    validBmp = photo.isImage();
+  picture.open(picFile);
 
-    return validBmp;
+  validBmp = photo.isImage();
 
-  }
+  return validBmp;
+
+}
 
 
 bool correctSize(string picFile1, string picFile2)
 
 {
-    Bitmap p1;
-    Bitmap p2;
-    
-    p1.open(picFile1);
-    p2.open(picFile2);
+  Bitmap p1;
+  Bitmap p2;
 
-    int correctHeight1 = xSize(p1);
-    int correctWidth1 = ySize(p1);
-    
-    int correctHeight2 = xSize(p2);
-    int correctWidth2 = ySize(p2);
+  p1.open(picFile1);
+  p2.open(picFile2);
 
+  int correctHeight1 = xSize(p1);
+  int correctWidth1 = ySize(p1);
 
-    if(correctHeight1 == correctHeight2)
-
-    {
-    
-      return true;
-      
-      }
+  int correctHeight2 = xSize(p2);
+  int correctWidth2 = ySize(p2);
 
 
-    
-    if(correctWidth1 == correctWidth2)
+  if(correctHeight1 == correctHeight2)
 
-    {
+  {
 
     return true;
 
-    }
+  }
 
-    else
-    
-    {
+
+
+  if(correctWidth1 == correctWidth2)
+
+  {
+
+    return true;
+
+  }
+
+  else
+
+  {
 
     return false;
 
-    }
+  }
 
-
+}
 
 int xSize(Bitmap x)
 
 {
-    vector <vector <Pixel> > row = x.toPixelMatrix();
-    
-    return row.size();
+  vector <vector <Pixel> > row = x.toPixelMatrix();
 
- }
-
-
-
- int ySize(Bitmap y)
-
- {
-
-    vector <vector <Pixel> > column = y.toPixelMatrix();
-    return column.size();
-
- }
+  return row.size();
 
 }
 
-    vector <vector <Pixel> > avgRgb( vector <string> picRgb)
+
+
+int ySize(Bitmap y)
 
 {
 
-    vector < vector <Pixel> > photoComb;
-    vector < vector <Pixel> > photoAvg;
-    Bitmap bmpComb1;
-    Bitmap bmpComb2;
-    Pixel rgb1;
-    Pixel rgb2;
+  vector <vector <Pixel> > column = y.toPixelMatrix();
+  return column.size();
 
-    bmpComb1.open(picList[0]);
-    photoComb = bmpComb1.toPixelMatrix();
+}
 
-    for(int x = 0; x < bmp.size(); x++)
+
+vector <vector <Pixel> > avgRgb( vector <string> picRgb)
+
+{
+
+  vector < vector <Pixel> > photoComb;
+  vector < vector <Pixel> > photoAvg;
+  Bitmap bmpComb1;
+  Bitmap bmpComb2;
+  Pixel rgb1;
+  Pixel rgb2;
+
+  bmpComb1.open(picList[0]);
+  photoComb = bmpComb1.toPixelMatrix();
+
+  for(int x = 0; x < bmp.size(); x++)
+  {
+
+    bmpComb2.open(picList[x]);
+    photoAvg = bmpComb2.toPixelMatrix();
+  } 
+  for(int y = 0; y < photoComb.size(); y++)
+
+  {
+
+
+    for (int f = 0; f < picList[y].size(); f++);
+
     {
 
-        bmpComb2.open(picList[x]);
-        photoAvg = bmpComb2.toPixelMatrix();
+      rgb1 = photoComb[x][y];
+      rgb2 = photoAvg[x][y];
 
-        for(int y = 0; y < photoComb.size(); y++)
-
-        {
-
-
-        for (int f = 0; f < picList[y].size(); f++);
-
-        {
-
-            rgb1 = photoComb[x][y];
-            rgb2 = photoAvg[x][y];
-
-            rgb1.red = rgb1.red +rgb2.red;
-            rgb1.green = rgb1.green + rgb2.green;
-            rgb1.blue = rgb1.blue + rgb2.blue;
-            photoComb[x][y] = rgb1;
-            
-        }
-
-        }
-        cout<<"The file "<< x+1<<" of "<<picList.size()<<" has been combined."<<endl;
+      rgb1.red = rgb1.red +rgb2.red;
+      rgb1.green = rgb1.green + rgb2.green;
+      rgb1.blue = rgb1.blue + rgb2.blue;
+      photoComb[x][y] = rgb1;
 
     }
 
-    return photoComb;
- 
+  }
+  cout<<"The file "<< x+1<<" of "<<picList.size()<<" has been combined."<<endl;
+
+}
+
+return photoComb;
+
 
 }
 
